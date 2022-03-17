c128a98d6afdfc4e885ea0503fa2c474b627e481
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-03-17 07:35:42 +0530

    junior assistant professors now get scraped

--- scraper/spiders/prof_spider.py
+++ scraper/spiders/prof_spider.py
@@ -9,7 +9,6 @@ class ProfSpider(scrapy.Spider):
         urls = [
             'https://chennai.vit.ac.in/computer-science-engineering-chennai/faculty/',
             'https://chennai.vit.ac.in/academics/schools/sense/faculty/'
-
         ]
         for url in urls:
             yield scrapy.Request(url = url, callback = self.parse)
@@ -37,25 +36,35 @@ class IndividualSpider(scrapy.Spider):
 
     def parse(self, response):
         p = response.css("div#content div.pure-content p")
-        prof = {
-            "name": response.css("div#content h3.item-title::text").get(),
-            "img": response.css("div#content article.single-event-content img::attr(src)")[1].extract(),
-            "designation": response.css("div#content h4.small-text::text").get(),
-            "email": p[1].get()
-                .replace('<p><strong>Email: </strong>', '')
-                .replace('<p><strong>Email:</strong>', '')
-                .replace('</p>', ''),
-            "PhD": p[2].get()
-                .replace('<p><strong>PhD: </strong>', '')
-                .replace('<p><strong>PhD:</strong>', '')
-                .replace('</p>', '')
-                .lstrip(),
-            "Research Area": p[3].get()
-                .replace('<p><strong>Research Area: </strong>', '')
-                .replace('<p><strong>Research Area:</strong>', '')
-                .replace('</p>', '')
-                .lstrip()
-        }
-        print(json.dumps(prof, indent = 4))
+        if (len(p) == 4):
+            prof = {
+                "name": response.css("div#content h3.item-title::text").get(),
+                "img": response.css("div#content article.single-event-content img::attr(src)")[1].extract(),
+                "designation": response.css("div#content h4.small-text::text").get(),
+                "email": p[1].get()
+                    .replace('<p><strong>Email: </strong>', '')
+                    .replace('<p><strong>Email:</strong>', '')
+                    .replace('</p>', ''),
+                "PhD": p[2].get()
+                    .replace('<p><strong>PhD: </strong>', '')
+                    .replace('<p><strong>PhD:</strong>', '')
+                    .replace('</p>', '')
+                    .lstrip(),
+                "Research Area": p[3].get()
+                    .replace('<p><strong>Research Area: </strong>', '')
+                    .replace('<p><strong>Research Area:</strong>', '')
+                    .replace('</p>', '')
+                    .lstrip()
+            }
+        else:
+            prof = {
+                "name": response.css("div#content h3.item-title::text").get(),
+                "img": response.css("div#content article.single-event-content img::attr(src)")[1].extract(),
+                "designation": response.css("div#content h4.small-text::text").get(),
+                "email": '',
+                "PhD": '',
+                "Research Area": ''
+            }
+        print(prof)
         file = open("profs.txt", 'a')
         file.write(json.dumps(prof, indent = 4) + ",\n")
